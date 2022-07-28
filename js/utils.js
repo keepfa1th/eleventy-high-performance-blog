fa980f802999310df7910a3d5df7b2f512e0649e
frantw <wei840220@gmail.com>
2022-07-28 21:45:08 +0000

    Deploying to gh-pages from @ frantw/eleventy-high-performance-blog@378b79ce20978988805f592f79821fe03ae79fc3 🚀

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,2 @@
-!function(){"use strict";window.addEventListener("load",()=>{let e=localStorage.getItem("isDarkTheme");e=null===e?window.matchMedia("(prefers-color-scheme: dark)").matches:"true"===e,document.body.setAttribute("class",!e||"dark-mode"),localStorage.setItem("isDarkTheme",!e&&"light-mode")})}();
+!function(){"use strict";let e=localStorage.getItem("isDarkTheme");e=null===e?window.matchMedia("(prefers-color-scheme: dark)").matches:"true"===e,document.body.setAttribute("class",e?"dark-mode":"light-mode"),localStorage.setItem("isDarkTheme",!!e)}();
 //# sourceMappingURL=utils.js.map
