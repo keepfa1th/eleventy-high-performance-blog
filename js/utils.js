39ede5ef869d19ac3e9a10e60ebf4f92f96415ab
frantw <wei840220@gmail.com>
2022-07-29 07:45:21 +0000

    Deploying to gh-pages from @ frantw/eleventy-high-performance-blog@23f9b95c7a78f0bf02197d5258fa6310148a8f8b 🚀

--- js/utils.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";let e=localStorage.getItem("isDarkTheme");e=null===e?window.matchMedia("(prefers-color-scheme: dark)").matches:"true"===e,document.body.setAttribute("class",e?"dark-mode":"light-mode"),localStorage.setItem("isDarkTheme",!!e)}();
-//# sourceMappingURL=utils.js.map
